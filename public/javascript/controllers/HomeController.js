d0ea3d26d678064a6e49947e4de824d3d4976fa2
Kandianne <kandipierre@gmail.com>
2016-02-01 00:25:59 -0800

    active button styling and functionality working

--- public/javascript/controllers/HomeController.js
+++ public/javascript/controllers/HomeController.js
@@ -3,8 +3,13 @@
 	angular.module('app')
 	.controller('HomeController', HomeController);
 
-	function HomeController() {
+	function HomeController($location, $scope) {
 		var vm = this;
 		vm.title = 'Bittyburb';
+
+		$scope.isCurrentPath = function (path) {
+			console.log("we getting here");
+			return $location.path() == path;
+		};
 	}
 })();
